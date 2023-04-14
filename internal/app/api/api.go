d75bfea23d8d230eff0b940bb5db870540ca4b2e
Vyacheslav Utrobin <utrobinslavik@gmail.com>
2023-04-14 12:12:27 +0500

    bd connect

--- internal/app/api/api.go
+++ internal/app/api/api.go
@@ -3,14 +3,16 @@ package api
 import (
 	"net/http"
 
+	"github.com/S5477/go-simple-server/storage"
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
 )
 
 type API struct {
-	config *Config
-	logger *logrus.Logger
-	router *mux.Router
+	config  *Config
+	logger  *logrus.Logger
+	router  *mux.Router
+	storage *storage.Storage
 }
 
 func New(config *Config) *API {
@@ -27,6 +29,9 @@ func (api *API) Run() error {
 	}
 
 	api.configureRouterField()
+	if err := api.configureStorageField(); err != nil {
+		return err
+	}
 
 	return http.ListenAndServe(api.config.BindAddr, api.router)
 }
