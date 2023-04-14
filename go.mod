18e2e32a90cb465c5bf1c29f6e06e071c6d05817
Vyacheslav Utrobin <utrobinslavik@gmail.com>
2023-04-14 10:37:54 +0500

    logger

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/S5477/go-simple-server
 
 go 1.20
 
-require github.com/BurntSushi/toml v1.2.1 // indirect
+require (
+	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
+)
--- internal/app/api/api.go
+++ internal/app/api/api.go
@@ -1,15 +1,25 @@
 package api
 
+import "github.com/sirupsen/logrus"
+
 type API struct {
 	config *Config
+	logger *logrus.Logger
 }
 
 func New(config *Config) *API {
 	return &API{
 		config: config,
+		logger: logrus.New(),
 	}
 }
 
 func (api *API) Run() error {
+	if err := api.configureLoggerField(); err != nil {
+		return err
+	}
+
+	api.logger.Info("starting api server at port:", api.config.BindAddr)
+
 	return nil
 }
