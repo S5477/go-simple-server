51164a9e9068b4574f2084d65cc0d8690db47c65
Vyacheslav Utrobin <utrobinslavik@gmail.com>
2023-04-14 10:21:15 +0500

    first

---
+++ cmd/api/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"github.com/BurntSushi/toml"
+	"github.com/S5477/go-simple-server/internal/app/api"
+)
+
+var (
+	configPath string
+)
+
+func init() {
+	flag.StringVar(&configPath, "path", "configs/api.toml", "path to cinfig file")
+}
+
+func main() {
+	flag.Parse()
+	log.Println("Hello, World!")
+	config := api.NewConfig()
+
+	_, err := toml.DecodeFile(configPath, config)
+
+	if err != nil {
+		log.Println("can not find configs file. using default values: ", err)
+	}
+
+	server := api.New(config)
+
+	if err := server.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/S5477/go-simple-server
+
+go 1.20
+
+require github.com/BurntSushi/toml v1.2.1 // indirect
